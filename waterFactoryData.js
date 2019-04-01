273c0e6e39a4665936229de14f42b9327401a556
Tung Duong <tungduonghgg123@gmail.com>
2019-04-01 20:48:44 +0700

    update data

--- waterFactoryData.js
+++ waterFactoryData.js
@@ -1,270 +1,376 @@
 ﻿var factories = [
         {
+            "district": "Tây Hồ",
             "name": "Công ty Nước sạch Hà Nội",
-            "location": "21.0461759,105.8437157"
+            "location": "21.0461759,105.8437157",
+            "address": "44 Yên Phụ, Trúc Bạch, Ba Đình, Hà Nội, Việt Nam"
         },
         {
             "name": "Công Ty Tnhh Mtv Thoát Nước Hà Nội - Xn Số 1",
-            "location": "21.0447879,105.8144492"
+            "location": "21.0447879,105.8144492",
+            "address": "284 Thụy Khuê, Thuỵ Khuê, Tây Hồ, Hà Nội, Việt Nam"
         },
         {
+            "district": "Ba Đình",
             "name": "Công Ty Nước Sạch Hà Nội - \r\nTrung Tâm Điều Hành Nước Sạch Quận Ba Đình",
-            "location": "21.0297022,105.8230859"
+            "location": "21.0297022,105.8230859",
+            "address": "18 Núi Trúc, Giảng Võ, Ba Đình, Hà Nội, Việt Nam"
         },
         {
             "name": "Công ty Nước sạch Hà Nội",
-            "location": "21.0461759,105.8437157"
+            "location": "21.0461759,105.8437157",
+            "address": "44 Yên Phụ, Trúc Bạch, Ba Đình, Hà Nội, Việt Nam"
         },
         {
             "name": "Trạm bơm nước Thành Công",
-            "location": "21.0227747,105.8138859"
+            "location": "21.0227747,105.8138859",
+            "address": "Đường La Thành, Thành Công, Ba Đình, Hà Nội, Việt Nam"
         },
         {
+            "district": "Hai Bà Trưng",
             "name": "Xí Nghiệp Kinh Doanh Nước Sạch Hai Bà Trưng",
-            "location": "21.0094412,21.0094412,"
+            "location": "21.0094412,21.0094412,",
+            "address": "1, Đường Trần Khát Chân, Phường Thanh Lương, Quận Hai Bà Trưng, Thành Phố Hà Nội, Đống Mác, Hai Bà Trưng, Hà Nội, Việt Nam"
         },
         {
             "name": "Nhà máy nước Lương Yên",
-            "location": "21.0096696,105.8618993"
+            "location": "21.0096696,105.8618993",
+            "address": "283/2 Trần Khát Chân, Đống Mác, Hai Bà Trưng, Hà Nội, Việt Nam"
         },
         {
             "name": "Nhà máy nước Tương Mai",
-            "location": "20.9870412,105.8537999"
+            "location": "20.9870412,105.8537999",
+            "address": "Nguyễn Đức Cảnh, Tương Mai, Hai Bà Trưng, Hà Nội"
         },
         {
             "name": "Trạm Cấp Nước Nguyễn Công Trứ",
-            "location": "21.0127271,105.853073"
+            "location": "21.0127271,105.853073",
+            "address": "H4 Nguyễn Công Trứ, Phố Huế, Hai Bà Trưng, Hà Nội"
         },
         {
+            "district": "Thanh Xuân",
             "name": "Công ty Cổ phần VIWACO",
-            "location": "21.0067328,105.8042729"
+            "location": "21.0067328,105.8042729",
+            "address": "17 Nguyễn Thị Thập, Trung Hoà, Thanh Xuân, Hà Nội"
         },
         {
             "name": "Nhà Máy Nước Hạ Đình",
-            "location": "20.9880535,105.8079737"
+            "location": "20.9880535,105.8079737",
+            "address": "14 Hạ Đình, Thanh Xuân, Hà Nội, Việt Nam"
         },
         {
             "name": "Công Ty Cổ Phần Xây Dựng Cấp Thoát Nước Số 1",
-            "location": "20.9924846,105.8108497"
+            "location": "20.9924846,105.8108497",
+            "address": "56 Ngõ 85, Hạ Đình, Quận Thanh Xuân, Thành Phố Hà Nội, Thanh Xuân Trung, Thanh Xuân, Hà Nội, Việt Nam"
         },
         {
             "name": "Trạm Nước Kim Giang",
-            "location": "20.9846051,105.8121304"
+            "location": "20.9846051,105.8121304",
+            "address": "Trạm Nước Kim Giang, Kim Giang, Thanh Xuân, Hà Nội, Việt Nam"
         },
         {
+            "district": "Đông Anh",
             "name": "Nhà máy nước Bắc Thăng Long",
-            "location": "21.1205487,105.7661952"
+            "location": "21.1205487,105.7661952",
+            "address": "H., Liên xã, Kim Chung, Đông Anh, Hà Nội, Việt Nam"
         },
         {
             "name": "Trạm bơm DPS (20m3/s)",
-            "location": "21.110314,105.7942249"
+            "location": "21.110314,105.7942249",
+            "address": "Hải Bối, Đông Anh, Hà Nội, Việt Nam"
         },
         {
+            "district": "Sóc Sơn",
             "name": "Trạm Nước Sạch Sóc Sơn",
-            "location": "21.252621,105.848812"
+            "location": "21.252621,105.848812",
+            "address": "tt., Tiên Dược, Sóc Sơn, Hà Nội, Việt Nam"
         },
         {
+            "district": "Mê Linh"
+        },
+        {
+            "district": "Đan Phượng",
             "name": "Trạm bơm Đan Hoài",
-            "location": "21.1128254,105.7212109"
+            "location": "21.1128254,105.7212109",
+            "address": "Liên Hà, Đan Phượng, Hà Nội, Việt Nam"
         },
         {
             "name": "Nhà máy cung cấp nước sạch Phường Thượng Cát",
-            "location": "21.0904813,105.7330173"
+            "location": "21.0904813,105.7330173",
+            "address": "Kỳ Vũ, Thượng Cát, Từ Liêm, Hà Nội, Việt Nam"
         },
         {
             "name": "Nhà Máy Nước Sạch Thị Trấn Phùng",
-            "location": "21.0879887,105.6725228"
+            "location": "21.0879887,105.6725228",
+            "address": "Khu Đồng Xây, Đan Phượng, Hà Nội, Việt Nam"
         },
         {
+            "district": "Từ Liêm",
             "name": "Nhà máy cung cấp nước sạch Phường Thượng Cát",
-            "location": "21.0904813,105.7330173"
+            "location": "21.0904813,105.7330173",
+            "address": "Kỳ Vũ, Thượng Cát, Từ Liêm, Hà Nội, Việt Nam"
         },
         {
             "name": "Công ty cổ phần cấp nước sạch Việt Nam",
-            "location": "21.0042899,105.7770988"
+            "location": "21.0042899,105.7770988",
+            "address": "Số 10 ĐCT08, Mễ Trì, Từ Liêm, Hà Nội, Việt Nam"
         },
         {
             "name": "Xí Nghiệp Kinh Doanh Nước Sạch Cầu Giấy",
-            "location": "21.0438754,105.7944042"
+            "location": "21.0438754,105.7944042",
+            "address": "121 Nghĩa Tân, Khu tập thể Nghĩa Tân, Cầu Giấy, Hà Nội"
         },
         {
+            "district": "Hoài Đức",
             "name": "Công ty CP Nước sạch Tây Hà Nội",
-            "location": "21.0372054,105.7331616"
+            "location": "21.0372054,105.7331616",
+            "address": "Số 40, LK 38, Khu ĐT Vân Canh - Hud, Hoài Đức, Hà Nội 10500, Việt Nam"
         },
         {
+            "district": "Mỹ Đức",
             "name": "Trạm Cấp Nước Sạch Hương Sơn, Công Ty Cấp Nước Hà Đông",
-            "location": "20.6204315,105.7762814"
+            "location": "20.6204315,105.7762814",
+            "address": "Hương Sơn, Mỹ Đức, Hà Nội, Việt Nam"
         },
         {
             "name": "Công ty cổ phần Nước và Hạ tầng kỹ thuật Hà Nội",
-            "location": "21.0455916,105.7930003"
+            "location": "21.0455916,105.7930003",
+            "address": "347 Hoàng Quốc Việt, Nghĩa Tân, Cầu Giấy, Hà Nội, Việt Nam"
         },
         {
+            "district": "Ứng Hòa",
             "name": "Xí Nghiệp Nước Sạch Ứng Hòa",
-            "location": "20.7314131,105.7734238"
+            "location": "20.7314131,105.7734238",
+            "address": "47B Ngõ 42 Quang Trung, TT. Vân Đình, Ứng Hòa,Hà Nội"
         },
         {
+            "district": "Phú Xuyên",
             "name": "Công Ty Tnhh Mtv Nước Sạch Hà Đông - Xí Nghiệp Nước Sạch Phú Xuyên",
-            "location": "20.743648,105.9172368"
+            "location": "20.743648,105.9172368",
+            "address": "QL1A, TT. Phú Xuyên, Phú Xuyên, Hà Nội, Việt Nam"
         },
         {
             "name": "Trạm Cấp Nước Phú Minh",
-            "location": "20.7788765,105.9132303"
+            "location": "20.7788765,105.9132303",
+            "address": "Đường Không Tên, Vạn Điểm, Phú Xuyên, Hà Nội, Việt Nam"
         },
         {
+            "district": "Thường Tín"
+        },
+        {
+            "district": "Thanh Oai",
             "name": "Trạm bơm thạch nham",
-            "location": "20.8643542,105.7703287"
+            "location": "20.8643542,105.7703287",
+            "address": "Mỹ hưng Huyện Thanh Oai, TT. Kim Bài, Thanh Oai, HN"
         },
         {
             "name": "Nhà máy nước sạch Thanh Oai",
-            "location": "20.8559893,105.7624067"
+            "location": "20.8559893,105.7624067",
+            "address": "TT. Kim Bài, Thanh Oai, Hà Nội, Việt Nam"
         },
         {
+            "district": "Chương Mỹ"
+        },
+        {
+            "district": "Ba Vì",
             "name": "Nhà máy nước Sông Đà - Ba Vì",
-            "location": "21.2271784,105.3595291"
+            "location": "21.2271784,105.3595291",
+            "address": "Phú Nhiêu, Ba Vì, Hà Nội, Việt Nam"
         },
         {
+            "district": "Sơn Tây",
             "name": "Trạm Xử Lý Nước Số 2 Công Ty Cấp Nước Sơn Tây - Nhà Máy Xử Lý Nước Số 1",
-            "location": "21.1419645,105.5104315"
+            "location": "21.1419645,105.5104315",
+            "address": "Cổng Ô, Viên Sơn, Sơn Tây, Hà Nội, Việt Nam"
         },
         {
+            "district": "Phúc Thọ"
+        },
+        {
+            "district": "Thạch Thất"
+        },
+        {
+            "district": "Hoài Đức",
             "name": "Công ty CP Nước sạch Tây Hà Nội",
-            "location": "21.0372054,105.7331616"
+            "location": "21.0372054,105.7331616",
+            "address": "Số 40, LK 38, Khu ĐT Vân Canh - Hud, Hoài Đức, Hà Nội 10500, Việt Nam"
         },
         {
+            "district": "Quốc Oai",
             "name": "Trạm Bơm Cấn Hạ",
-            "location": "20.944743,105.6203528"
+            "location": "20.944743,105.6203528",
+            "address": "Trạm Bơm Yên Sở Hoàng Mai, Cấn Huu, Quốc Oai, Hà Nội, Việt Nam"
         },
         {
             "name": "NHÀ MÁY NƯỚC MẶT SÔNG ĐUỐNG",
-            "location": "21.0575233,105.9765334"
+            "location": "21.0575233,105.9765334",
+            "address": "Phù Đổng, Gia Lâm, Hà Nội, Việt Nam"
         },
         {
+            "district": "Long Biên",
             "name": "Công Ty Cổ phần Nước Sạch Số 2 Hà Nội",
-            "location": "21.0477165,21.0477165"
+            "location": "21.0477165,21.0477165",
+            "address": "19 Nguyễn Văn Linh, Việt Hưng, Long Biên, Hà Nội"
         },
         {
             "name": "Xí Nghiệp Thoát Nước Số 5",
-            "location": "21.034179,105.9054939"
+            "location": "21.034179,105.9054939",
+            "address": "449 Nguyễn Văn Linh, Phúc Đồng, Long Biên, Hà Nội"
         },
         {
             "name": "       Nhà Máy Nước Khu Công Nghiệp Sài Đồng B",
-            "location": "21.0260893,105.9007094"
+            "location": "21.0260893,105.9007094",
+            "address": "Công Nghiệp 3, Long Biên, Hà Nội, Việt Nam"
         },
         {
+            "district": "Hoàng Mai",
             "name": "Xí nghiệp nước sạch Hoàng Mai",
-            "location": "20.970561,105.8460725"
+            "location": "20.970561,105.8460725",
+            "address": "ngõ 1141 Giải Phóng, Thịnh Liệt, Hoàng Mai, Hà Nội"
         },
         {
             "name": "Trạm bơm Yên Sở",
-            "location": "20.9573469,105.8702016"
+            "location": "20.9573469,105.8702016",
+            "address": "Nguyễn Khoái, Yên Sở, Hoàng Mai, Hà Nội, Việt Nam"
         },
         {
             "name": "Công ty CP thương mại xây dựng cấp thoát nước",
-            "location": "20.97253,105.828005"
+            "location": "20.97253,105.828005",
+            "address": "Số 1, OCT2, khu đô thị mới Bắc Linh Đàm, Đại Kim, Hoàng Mai, Hà Nội, Việt Nam"
         },
         {
             "name": "Công Ty Tnhh Công Nghệ Môi Trường Và Tự Động Hóa Minh Tân",
-            "location": "20.978967,105.820302"
+            "location": "20.978967,105.820302",
+            "address": "Số 2, 16 Ngõ 250, Ngõ 64 Đường Kim Giang, Hà Nội, Kim Giang\\nHoàng Mai\\nHà Nội 100000, Việt Nam"
         },
         {
             "name": "Trạm cấp nước Linh Đàm",
-            "location": "20.9622912,105.8274257"
+            "location": "20.9622912,105.8274257",
+            "address": "Linh Đường, Hoàng Liệt, Hoàng Mai, Hà Nội,"
         },
         {
             "name": "Xí Nghiệp Thoát Nước Số 3",
-            "location": "20.9738023,105.8878934"
+            "location": "20.9738023,105.8878934",
+            "address": "Nguyễn Khoái, Trần Phú, Hoàng Mai, Hà Nội"
         },
         {
             "name": "Nhà máy nước Nam Dư",
-            "location": "20.975339,105.8873824"
+            "location": "20.975339,105.8873824",
+            "address": "ngõ 9 đường khuyến lương- trần phú- hoàng mai- hanoi, Trần Phú, Hoàng Mai, Hà Nội, Việt Nam"
         },
         {
             "name": "Trạm Cấp Nước Đền Lừ",
-            "location": "20.9845813,105.8589363"
+            "location": "20.9845813,105.8589363",
+            "address": "32ho Đền Lừ, Hoàng Văn Thụ, Hoàng Mai, Hà Nội "
         },
         {
             "name": "Đối diện trạm cấp nước sạch Định Công",
-            "location": "20.9875679,105.8317667"
+            "location": "20.9875679,105.8317667",
+            "address": "Khu đô thị Định Công, Hoàng Mai, Hà Nội, Việt Nam"
         },
         {
             "name": "Trạm Cấp Nước Sạch Khu Đô Thị Mới Định Công",
-            "location": "20.9877167,105.8315992"
+            "location": "20.9877167,105.8315992",
+            "address": "Trần Nguyên Đán, Định Công, Hoàng Mai, Hà Nội"
         },
         {
             "name": "Nhà máy nước Pháp Vân",
-            "location": "20.9648174,105.8462754"
+            "location": "20.9648174,105.8462754",
+            "address": "Hoàng Liệt, Hoàng Mai, Hà Nội, Việt Nam"
         },
         {
+            "district": "Thanh Trì",
             "name": "Khu tập thể Cấp thoát nước",
-            "location": "20.9139003,105.8537757"
+            "location": "20.9139003,105.8537757",
+            "address": "Km 14,5 QL1A, Liên Ninh, Thanh Trì, Hà Nội"
         },
         {
             "name": "Trạm Cấp Nước Sạch Tựu Liệt",
-            "location": "20.9530125,105.8342725"
+            "location": "20.9530125,105.8342725",
+            "address": "192, Tựu Liệt, Thị Trấn Văn Điển, Huyện Thanh Trì,HN"
         },
         {
             "name": "Xí Nghiệp Thoát Nước Số 7",
-            "location": "20.9591303,105.8041978"
+            "location": "20.9591303,105.8041978",
+            "address": "53 Cầu Bươu, Tân Triều, Hà Đông, Hà Nội, Việt Nam"
         },
         {
             "name": "Nhà máy nước sạch Đông Mỹ",
-            "location": "20.9165646,105.8736462"
+            "location": "20.9165646,105.8736462",
+            "address": "Đông Phù, Thanh Trì, Hà Nội, Việt Nam"
         },
         {
+            "district": "Hà Đông",
             "name": "Công Ty TNHH MTV Nước Sạch Hà Đông - Cơ Sở 2 Ba La",
-            "location": "20.9582829,105.760807"
+            "location": "20.9582829,105.760807",
+            "address": "797 Quang Trung, Phú La, Hà Đông, Hà Nội, Việt Nam"
         },
         {
             "name": "Công Ty Nước Sạch Hà Đông",
-            "location": "20.9721371,105.7786756"
+            "location": "20.9721371,105.7786756",
+            "address": "Số 4 Bà Triệu, P. Nguyễn Trãi, Hà Đông, Hà Nội"
         },
         {
             "name": "Công ty Cổ phần VIWACO",
-            "location": "21.0067328,105.8042729"
+            "location": "21.0067328,105.8042729",
+            "address": "17 Nguyễn Thị Thập, Trung Hoà, Thanh Xuân, Hà Nội"
         },
         {
             "name": "Công ty Nước sạch Hà Nội",
-            "location": "21.0461759,105.8437157"
+            "location": "21.0461759,105.8437157",
+            "address": "17 Nguyễn Thị Thập, Trung Hoà, Thanh Xuân, Hà Nội"
         },
         {
             "name": "Vinaconex Water Supply Jsc",
-            "location": "21.0069162,105.8043373"
+            "location": "21.0069162,105.8043373",
+            "address": "17t8 trung hoa nhan chinh, Trung Hoà, Cầu Giấy,HN"
         },
         {
             "name": "Xí nghiệp Thoát nước số 6",
-            "location": "20.9841784,105.788221"
+            "location": "20.9841784,105.788221",
+            "address": "LK6C, P. Mộ Lao, Hà Đông, Hà Nội"
         },
         {
             "name": "Công Ty Cổ phần Nước Sạch Số 2 Hà Nội",
-            "location": "21.0477165,105.8979651"
+            "location": "21.0477165,105.8979651",
+            "address": "19 Nguyễn Văn Linh, Việt Hưng, Long Biên, Hà Nội,"
         },
         {
+            "district": "Đống Đa",
             "name": "Xí nghiệp Thoát nước Số 2",
-            "location": "21.0045727,105.8174257"
+            "location": "21.0045727,105.8174257",
+            "address": "107 Đường Láng, Thượng Đình, Đống Đa, Hà Nội"
         },
         {
             "name": "Xí Nghiệp Thoát Nước Số 4",
-            "location": "21.0061967,105.8361151"
+            "location": "21.0061967,105.8361151",
+            "address": "1 Phố Lương Định Của, Kim Liên, Đống Đa, Hà Nội"
         },
         {
             "name": "Xí Nghiệp Kinh Doanh Nước Sạch Đống Đa - Trạm Bơm Tăng Áp Thành Công",
-            "location": "21.0228073,105.8140307"
+            "location": "21.0228073,105.8140307",
+            "address": "Thành Công, Ba Đình, Hà Nội, Việt Nam"
         },
         {
+            "district": "Hoàn Kiếm",
             "name": "Bql Dự Án Thoát Nước Hà Nội",
-            "location": "21.0271686,105.8425373"
+            "location": "21.0271686,105.8425373",
+            "address": "102 Hai Bà Trưng, Cửa Nam, Hoàn Kiếm, Hà Nội"
         },
         {
             "name": "Xí Nghiệp Kinh Doanh Nước Sạch Hoàn Kiếm",
-            "location": "21.0190906,105.8596937"
+            "location": "21.0190906,105.8596937",
+            "address": "8C, Phường Phan Chu Trinh, Quận Hoàn Kiếm, Đường Đinh Công Tráng, Thành Phố Hà Nội, Phan Chu Trinh, Hoàn Kiếm, Hà Nội, Việt Nam"
         },
         {
+            "district": "Cầu Giấy",
             "name": "Nhà Máy Nước Mai Dịch",
-            "location": "21.0323738,105.7806627"
+            "location": "21.0323738,105.7806627",
+            "address": "1 Phạm Hùng, Dịch Vọng Hậu, Cầu Giấy, Hà Nội"
         },
         {
             "name": "Xí Nghiệp Kinh Doanh Nước Sạch Cầu Giấy",
-            "location": "21.0438754,105.7944042"
+            "location": "21.0438754,105.7944042",
+            "address": "121 Nghĩa Tân, Khu tập thể Nghĩa Tân, Cầu Giấy,hn"
         }
-    ]
+]
