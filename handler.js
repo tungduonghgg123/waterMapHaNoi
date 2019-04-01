8c0d11eba0b746eb40fdc63987760cf53f9ee5ad
Tung Duong <tungduonghgg123@gmail.com>
2019-04-01 18:05:53 +0700

    use Git

---
+++ handler.js
@@ -0,0 +1,28 @@
+function distance(lat1,lng1,lat2,lng2) {
+	var R = 6371; // km (change this constant to get miles)
+	var dLat = (lat2-lat1) * Math.PI / 180;
+	var dLon = (lng2-lng1) * Math.PI / 180;
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+		Math.sin(dLon/2) * Math.sin(dLon/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var d = R * c;
+	// if (d>1) return Math.round(d)+"km";
+	// else if (d<=1) return Math.round(d*1000)+"m";
+	return Math.round(d);
+}
+function findNearestWaterFactory(lat, lon) {
+    var index;
+    var closest = 9999;
+    factories.forEach(({location}, i) => {
+        var lat1 = location.split(',')[0];
+        var lng2 = location.split(',')[1];
+        if(distance(lat,lon,lat1,lng2) < closest) {
+            closest = distance(lat,lon,lat1,lng2);
+            index = i;
+        }
+    });
+    return factories[index]
+}
+
+
---
+++ waterFactoryData.js
@@ -0,0 +1,270 @@
+﻿var factories = [
+        {
+            "name": "Công ty Nước sạch Hà Nội",
+            "location": "21.0461759,105.8437157"
+        },
+        {
+            "name": "Công Ty Tnhh Mtv Thoát Nước Hà Nội - Xn Số 1",
+            "location": "21.0447879,105.8144492"
+        },
+        {
+            "name": "Công Ty Nước Sạch Hà Nội - \r\nTrung Tâm Điều Hành Nước Sạch Quận Ba Đình",
+            "location": "21.0297022,105.8230859"
+        },
+        {
+            "name": "Công ty Nước sạch Hà Nội",
+            "location": "21.0461759,105.8437157"
+        },
+        {
+            "name": "Trạm bơm nước Thành Công",
+            "location": "21.0227747,105.8138859"
+        },
+        {
+            "name": "Xí Nghiệp Kinh Doanh Nước Sạch Hai Bà Trưng",
+            "location": "21.0094412,21.0094412,"
+        },
+        {
+            "name": "Nhà máy nước Lương Yên",
+            "location": "21.0096696,105.8618993"
+        },
+        {
+            "name": "Nhà máy nước Tương Mai",
+            "location": "20.9870412,105.8537999"
+        },
+        {
+            "name": "Trạm Cấp Nước Nguyễn Công Trứ",
+            "location": "21.0127271,105.853073"
+        },
+        {
+            "name": "Công ty Cổ phần VIWACO",
+            "location": "21.0067328,105.8042729"
+        },
+        {
+            "name": "Nhà Máy Nước Hạ Đình",
+            "location": "20.9880535,105.8079737"
+        },
+        {
+            "name": "Công Ty Cổ Phần Xây Dựng Cấp Thoát Nước Số 1",
+            "location": "20.9924846,105.8108497"
+        },
+        {
+            "name": "Trạm Nước Kim Giang",
+            "location": "20.9846051,105.8121304"
+        },
+        {
+            "name": "Nhà máy nước Bắc Thăng Long",
+            "location": "21.1205487,105.7661952"
+        },
+        {
+            "name": "Trạm bơm DPS (20m3/s)",
+            "location": "21.110314,105.7942249"
+        },
+        {
+            "name": "Trạm Nước Sạch Sóc Sơn",
+            "location": "21.252621,105.848812"
+        },
+        {
+            "name": "Trạm bơm Đan Hoài",
+            "location": "21.1128254,105.7212109"
+        },
+        {
+            "name": "Nhà máy cung cấp nước sạch Phường Thượng Cát",
+            "location": "21.0904813,105.7330173"
+        },
+        {
+            "name": "Nhà Máy Nước Sạch Thị Trấn Phùng",
+            "location": "21.0879887,105.6725228"
+        },
+        {
+            "name": "Nhà máy cung cấp nước sạch Phường Thượng Cát",
+            "location": "21.0904813,105.7330173"
+        },
+        {
+            "name": "Công ty cổ phần cấp nước sạch Việt Nam",
+            "location": "21.0042899,105.7770988"
+        },
+        {
+            "name": "Xí Nghiệp Kinh Doanh Nước Sạch Cầu Giấy",
+            "location": "21.0438754,105.7944042"
+        },
+        {
+            "name": "Công ty CP Nước sạch Tây Hà Nội",
+            "location": "21.0372054,105.7331616"
+        },
+        {
+            "name": "Trạm Cấp Nước Sạch Hương Sơn, Công Ty Cấp Nước Hà Đông",
+            "location": "20.6204315,105.7762814"
+        },
+        {
+            "name": "Công ty cổ phần Nước và Hạ tầng kỹ thuật Hà Nội",
+            "location": "21.0455916,105.7930003"
+        },
+        {
+            "name": "Xí Nghiệp Nước Sạch Ứng Hòa",
+            "location": "20.7314131,105.7734238"
+        },
+        {
+            "name": "Công Ty Tnhh Mtv Nước Sạch Hà Đông - Xí Nghiệp Nước Sạch Phú Xuyên",
+            "location": "20.743648,105.9172368"
+        },
+        {
+            "name": "Trạm Cấp Nước Phú Minh",
+            "location": "20.7788765,105.9132303"
+        },
+        {
+            "name": "Trạm bơm thạch nham",
+            "location": "20.8643542,105.7703287"
+        },
+        {
+            "name": "Nhà máy nước sạch Thanh Oai",
+            "location": "20.8559893,105.7624067"
+        },
+        {
+            "name": "Nhà máy nước Sông Đà - Ba Vì",
+            "location": "21.2271784,105.3595291"
+        },
+        {
+            "name": "Trạm Xử Lý Nước Số 2 Công Ty Cấp Nước Sơn Tây - Nhà Máy Xử Lý Nước Số 1",
+            "location": "21.1419645,105.5104315"
+        },
+        {
+            "name": "Công ty CP Nước sạch Tây Hà Nội",
+            "location": "21.0372054,105.7331616"
+        },
+        {
+            "name": "Trạm Bơm Cấn Hạ",
+            "location": "20.944743,105.6203528"
+        },
+        {
+            "name": "NHÀ MÁY NƯỚC MẶT SÔNG ĐUỐNG",
+            "location": "21.0575233,105.9765334"
+        },
+        {
+            "name": "Công Ty Cổ phần Nước Sạch Số 2 Hà Nội",
+            "location": "21.0477165,21.0477165"
+        },
+        {
+            "name": "Xí Nghiệp Thoát Nước Số 5",
+            "location": "21.034179,105.9054939"
+        },
+        {
+            "name": "       Nhà Máy Nước Khu Công Nghiệp Sài Đồng B",
+            "location": "21.0260893,105.9007094"
+        },
+        {
+            "name": "Xí nghiệp nước sạch Hoàng Mai",
+            "location": "20.970561,105.8460725"
+        },
+        {
+            "name": "Trạm bơm Yên Sở",
+            "location": "20.9573469,105.8702016"
+        },
+        {
+            "name": "Công ty CP thương mại xây dựng cấp thoát nước",
+            "location": "20.97253,105.828005"
+        },
+        {
+            "name": "Công Ty Tnhh Công Nghệ Môi Trường Và Tự Động Hóa Minh Tân",
+            "location": "20.978967,105.820302"
+        },
+        {
+            "name": "Trạm cấp nước Linh Đàm",
+            "location": "20.9622912,105.8274257"
+        },
+        {
+            "name": "Xí Nghiệp Thoát Nước Số 3",
+            "location": "20.9738023,105.8878934"
+        },
+        {
+            "name": "Nhà máy nước Nam Dư",
+            "location": "20.975339,105.8873824"
+        },
+        {
+            "name": "Trạm Cấp Nước Đền Lừ",
+            "location": "20.9845813,105.8589363"
+        },
+        {
+            "name": "Đối diện trạm cấp nước sạch Định Công",
+            "location": "20.9875679,105.8317667"
+        },
+        {
+            "name": "Trạm Cấp Nước Sạch Khu Đô Thị Mới Định Công",
+            "location": "20.9877167,105.8315992"
+        },
+        {
+            "name": "Nhà máy nước Pháp Vân",
+            "location": "20.9648174,105.8462754"
+        },
+        {
+            "name": "Khu tập thể Cấp thoát nước",
+            "location": "20.9139003,105.8537757"
+        },
+        {
+            "name": "Trạm Cấp Nước Sạch Tựu Liệt",
+            "location": "20.9530125,105.8342725"
+        },
+        {
+            "name": "Xí Nghiệp Thoát Nước Số 7",
+            "location": "20.9591303,105.8041978"
+        },
+        {
+            "name": "Nhà máy nước sạch Đông Mỹ",
+            "location": "20.9165646,105.8736462"
+        },
+        {
+            "name": "Công Ty TNHH MTV Nước Sạch Hà Đông - Cơ Sở 2 Ba La",
+            "location": "20.9582829,105.760807"
+        },
+        {
+            "name": "Công Ty Nước Sạch Hà Đông",
+            "location": "20.9721371,105.7786756"
+        },
+        {
+            "name": "Công ty Cổ phần VIWACO",
+            "location": "21.0067328,105.8042729"
+        },
+        {
+            "name": "Công ty Nước sạch Hà Nội",
+            "location": "21.0461759,105.8437157"
+        },
+        {
+            "name": "Vinaconex Water Supply Jsc",
+            "location": "21.0069162,105.8043373"
+        },
+        {
+            "name": "Xí nghiệp Thoát nước số 6",
+            "location": "20.9841784,105.788221"
+        },
+        {
+            "name": "Công Ty Cổ phần Nước Sạch Số 2 Hà Nội",
+            "location": "21.0477165,105.8979651"
+        },
+        {
+            "name": "Xí nghiệp Thoát nước Số 2",
+            "location": "21.0045727,105.8174257"
+        },
+        {
+            "name": "Xí Nghiệp Thoát Nước Số 4",
+            "location": "21.0061967,105.8361151"
+        },
+        {
+            "name": "Xí Nghiệp Kinh Doanh Nước Sạch Đống Đa - Trạm Bơm Tăng Áp Thành Công",
+            "location": "21.0228073,105.8140307"
+        },
+        {
+            "name": "Bql Dự Án Thoát Nước Hà Nội",
+            "location": "21.0271686,105.8425373"
+        },
+        {
+            "name": "Xí Nghiệp Kinh Doanh Nước Sạch Hoàn Kiếm",
+            "location": "21.0190906,105.8596937"
+        },
+        {
+            "name": "Nhà Máy Nước Mai Dịch",
+            "location": "21.0323738,105.7806627"
+        },
+        {
+            "name": "Xí Nghiệp Kinh Doanh Nước Sạch Cầu Giấy",
+            "location": "21.0438754,105.7944042"
+        }
+    ]
